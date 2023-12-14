e372a68db21f890762747e9c9ad851ff16282662
EdnaChA <ej.chaparro702@uniandes.edu.co>
2023-12-13 19:35:06 -0500

    Agregamos estilos

--- Funciones/Ejercicio3/funciones.js
+++ Funciones/Ejercicio3/funciones.js
@@ -1,18 +1,20 @@
 const botonSendRows = document.getElementById("send_filas")
 const rows = document.getElementById("filasIngresadas")
-
 var salida = document.getElementById("respuesta")
 
 botonSendRows.onclick = function(){ 
-    console.log(rows.value)
+    if(rows.value == 0){
+        alert("Ingrese un número mayor a cero")
+    }
     salida.innerHTML = dibujarTriangulo()
     
 }
 function dibujarTriangulo() {
     const numFilas = parseInt(rows.value);
     let triangulo = '';
+
     for (let i = 1; i <= numFilas; i++) {
-        triangulo += ' '.repeat(numFilas - i) + '*'.repeat(2 * i - 1) + '\n';
+        triangulo += ' '.repeat(numFilas - i) + '*'.repeat(2 * i - 1) + ' '.repeat(numFilas - i) + '\n';
     }
     return triangulo;
 }
